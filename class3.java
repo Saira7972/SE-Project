4d1e92c5f49102e03829704a1548214619c9d279
siyalamna <132810681+siyalamna@users.noreply.github.com>
2023-05-18 15:04:19 +0500

    Test project
    Amna,Saira assigment

---
+++ class3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class class3 {
+        public void display(){
+            Scanner myObj=new Scanner(System.in);
+            String NAME;
+            //Enter date of birth and press enter
+            System.out.println("DATE OF BIRTH:");
+            NAME =myObj.nextLine();
+            System.out.println("DATE OF BIRTH is :" +NAME);
+        }
+}
+
---
+++ class4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class class4 {
+        public void display(){
+            Scanner myObj=new Scanner(System.in);
+            String NAME;
+            //Enter address and press enter
+            System.out.println("YOUR ADDRESS:");
+            NAME =myObj.nextLine();
+            System.out.println("YOUR ADDRESS is :"+NAME);
+        }
+
+}
+
+
